c281f0263a1d41762c3a884cfcb93865a232d7d6
Administrator <osama9mohamed5@gmail.com>
2023-01-22 21:01:46 +0200

    edited edge script

--- Browsers/edge.sh
+++ Browsers/edge.sh
@@ -1,5 +1,6 @@
 sudo apt update && sudo apt upgrade
 cd ~/Downloads
+sudo apt install software-properties-common apt-transport-https wget curl
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
