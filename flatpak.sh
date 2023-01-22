ec8e158940a3aa6e183ba37aa4601ddec7478136
Administrator <osama9mohamed5@gmail.com>
2023-01-23 00:28:29 +0200

    added webapp and flatpak

--- flatpak.sh
+++ flatpak.sh
@@ -8,3 +8,16 @@ sudo apt install gnome-software-plugin-flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 # now restart 
 echo "Restart your pc now"
+
+
+# some useful apps
+
+# -------------------------------- otp-client -------------------------------- #
+# 1. download the .flatpak file 
+https://dl.flathub.org/repo/appstream/com.github.paolostivanin.OTPClient.flatpakref
+# 2. install it 
+flatpak install flathub com.github.paolostivanin.OTPClient
+# 3. run it
+flatpak run com.github.paolostivanin.OTPClient
+# 4. remove the .flatpak file
+rm com.github.paolostivanin.OTPClient.flatpakref
