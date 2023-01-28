9c23b73a5c9e68b25c7f27cbddaa57411f51fc5f
Administrator <osama9mohamed5@gmail.com>
2023-01-29 00:16:44 +0200

    edited the minikube to ask whether to use VB or docker

--- DevOps/minikube.sh
+++ DevOps/minikube.sh
@@ -29,14 +29,15 @@ if !(ls /bin | grep "docker" >/dev/null); then
     else
         echo "You need to install docker first"
         exit
-    if
+    fi
 fi
 
+# ask the user if he wants to use virtualbox or docker and create the readd var is vb_or_docker
+read -p "Do you want to use virtualbox or docker as a minikube driver? (vb/d) " vb_or_docker
 
 UB(){
     sudo apt update -y && sudo apt upgrade -y
     sudo apt install curl -y
-    sudo apt install virtualbox virtualbox-ext-pack -y
     wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
     sudo cp minikube-linux-amd64 /usr/local/bin/minikube
     sudo chmod 755 /usr/local/bin/minikube
@@ -60,8 +61,39 @@ elif [ -f /etc/redhat-release ]; then
     RH
 fi
 
+# check the version of minikube
 minikube version
+# Download and install kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 sudo chmod +x kubectl
 sudo mv kubectl /usr/local/bin/
-minikube start
+
+# --------- install virtualbox or docker depending on the user choice -------- #
+vb_docker_install(){
+    if [ -f /etc/debian_version ]; then
+        sudo apt install virtualbox virtualbox-ext-pack -y
+    elif [ -f /etc/redhat-release ]; then
+        # install virtualbox on centos for minikube
+        sudo yum install kernel-devel kernel-devel-$(uname -r) kernel-headers kernel-headers-$(uname -r) make patch gcc -y
+        sudo wget https://download.virtualbox.org/virtualbox/rpm/el/virtualbox.repo -P /etc/yum.repos.d
+        sudo yum install VirtualBox-5.2 -y
+    fi
+}
+
+# ------------ check if the user wants to use virtualbox or docker ----------- #
+if [ "$vb_or_docker" = "vb" ]; then
+    vb_docker_install
+    minikube config set driver virtualbox
+    minikube start --driver=virtualbox
+elif [ "$vb_or_docker" = "d" ]; then
+    vb_docker_install
+    minikube config set driver docker
+    minikube start --driver=docker
+else
+    echo
+    echo "No choice was made, installing docker"
+    # if no choice is made then install docker
+    vb_docker_install
+    minikube config set driver docker
+    minikube start --driver=docker
+fi 
